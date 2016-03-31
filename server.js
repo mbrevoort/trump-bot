8648ecac29deed4e9f9698e9a406187c7cdc98b4
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2016-03-31 14:24:23 -0600

    Add single team support too

--- server.js
+++ server.js
@@ -4,7 +4,28 @@ var witbot = Witbot(process.env.WIT_TOKEN)
 var getResponse = require('./responses')
 
 var controller = Botkit.slackbot({debug: false})
-require('beepboop-botkit').start(controller, { debug: true })
+var token = process.env.SLACK_TOKEN
+
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.on('rtm_close', function () {
+    console.log('rtm_close')
+  })
+  controller.on('rtm_reconnect_failed', function () {
+    console.log('RECONNECT failed')
+  })
+  controller.spawn({
+    token: token,
+    retry: Infinity
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+  })
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
 
 controller.hears('.*', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
   witbot.process(message.text).any(function (outcome) {
