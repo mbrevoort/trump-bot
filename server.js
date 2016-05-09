e3b1fe60fca70e1bd4ec60ca02abc3f2eac6194f
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2016-05-09 13:48:47 -0600

    retry intro

--- server.js
+++ server.js
@@ -40,13 +40,19 @@ if (bbb) {
         return console.log('Error looking up botkit bot for team %s', slackTeamId)
       }
 
-      console.log('starting private conversation with ', slackUserId)
-      bot.api.im.open({user: slackUserId}, function (err, response) {
-        if (err) return console.log(err)
-        var dmChannel = response.channel.id
-        bot.say({channel: dmChannel, text: 'I am the most glorious bot to join your team'})
-        bot.say({channel: dmChannel, text: 'You must now /invite me to a channel so that I may show everyone how dumb you are'})
-      })
+      var began = Date.now()
+      var trySendIntro = function () {
+        if (Date.now() > began + 30000) return console.log('Giving up after 30s to send intro')
+        if (!bot.rtm) return setTimeout(trySendIntro, 1000)
+        console.log('starting private conversation with ', slackUserId)
+        bot.api.im.open({user: slackUserId}, function (err, response) {
+          if (err) return console.log(err)
+          var dmChannel = response.channel.id
+          bot.say({channel: dmChannel, text: 'I am the most glorious bot to join your team'})
+          bot.say({channel: dmChannel, text: 'You must now /invite me to a channel so that I may show everyone how dumb you are'})
+        })
+      }
+      trySendIntro()
     }
   })
 }
